e99dc81e5a821f7f19fa20efaa628ccfa1a85356
KomkridKommanee <125717048+KomkridKommanee@users.noreply.github.com>
2023-04-20 18:26:46 +0700

    decorate
    เพิ่มรูป

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -10,7 +10,7 @@ export default function Homepage() {
   // display
   return (
     <>
-    <UserNavbar/>
+      <UserNavbar />
       <main className=" bg-cover h-screen sm:px-5 md:px-[10%] xl:px-[20%]">
         <Layout className="h-full w-full bg-yellow-50">
           <Header className=' bg-transparent h-[10%] justify-end items-center flex'>
@@ -18,24 +18,28 @@ export default function Homepage() {
           <Content className='h-[70%] '>
             <Content className='flex justify-center items-center h-full '>
               <div className='text-center font-itim bigger-font '>
-              <div className='sm:px-0 md:px-[5%] xl:px-[10%] flex justify-center items-center bg-yellow-400 shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] px-10 w-auto h-1/2 border-dashed rounded-lg  font-itim bigger-font '>
-                Welcome to Perfood เชียงคาน
+                <div className='sm:px-0 md:px-[5%] xl:px-[10%] flex justify-center items-center bg-yellow-400 shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] px-10 w-auto h-1/2 border-dashed rounded-lg  font-itim bigger-font '>
+
+                  <img class="h-auto max-w-full fixed  " src='/line3.png' alt="image description"/>
+                    Welcome to Perfood เชียงคาน
+                    
+
                 </div>
               </div>
             </Content>
           </Content>
           <Footer className='bg-transparent h-[20%] item-center px-10 flex'>
             <div className='justify-center w-full flex  items-center  foot-font'>
-            <div className=' border-dashed rounded-lg justify-center w-[49%] px-3 flex items-center  foot-font'>
-              <Link className=' items-center w-auto main-icon flex justify-center ' href="/Playgame">
-              <button type="button" class="text-white bg-gradient-to-br from-yellow-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-200 dark:focus:ring-yellow-800 font-extrabold rounded-lg text-base px-6 py-3.5 text-center mr-2 mb-2 inline-flex">
-              <svg class="w-10 h-10 mr-5 -ml-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"></path>
-              </svg>
-              ลองเล่นเกมของเราสิ
-              </button>
-              
-              </Link>
+              <div className=' border-dashed rounded-lg justify-center w-[49%] px-3 flex items-center  foot-font'>
+                <Link className=' items-center w-auto main-icon flex justify-center ' href="/Playgame">
+                  <button type="button" class="text-white bg-gradient-to-br from-yellow-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-200 dark:focus:ring-yellow-800 font-extrabold rounded-lg text-base px-6 py-3.5 text-center mr-2 mb-2 inline-flex">
+                    <svg class="w-10 h-10 mr-5 -ml-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"></path>
+                    </svg>
+                    ลองเล่นเกมของเราสิ
+                  </button>
+
+                </Link>
               </div>
             </div>
           </Footer>
