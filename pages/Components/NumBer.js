7b082cb7d719a3d18517bcb4d9e7a9f73d17d6f7
KomkridKommanee <125717048+KomkridKommanee@users.noreply.github.com>
2023-04-30 23:48:19 +0700

    Update NumBer.js

--- pages/Components/NumBer.js
+++ pages/Components/NumBer.js
@@ -5,23 +5,23 @@ function NumBer() {
     
 
     return (
-        <div className='  w-full h-full shadow-md'>
+        <div className='  w-full h-full '>
             {/* Doughnut chart */}
             {/* <h1 className="w-[150px] mx-auto mt-10 text-xl font-semibold capitalize ">Doughnut Chart</h1> */}
 
-                <Row className='  w-full h-1/4 '>
-                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 radio-font text-center text-gray-500'>
+                <Row className='  w-full h-1/4 shadow-lg'>
+                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 radio-font text-center  text-yellow-800'>
                     จำนวนผู้เข้าชมที่ไม่ตอบแบบสอบถาม
                 </Col>
-                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 radio-font text-center text-gray-500'>
+                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 radio-font text-center  text-yellow-800'>
                     จำนวนผู้เข้าชมที่ตอบแบบสอบถาม
                 </Col>
                 </Row>
-                <Row className="w-full h-3/4 border-solid ">
-                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 foot-font text-center '>
+                <Row className="w-full h-3/4 shadow-lg">
+                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 foot-font text-center text-yellow-800'>
                     0
                 </Col>
-                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 foot-font text-center '>
+                <Col className='pt-0  w-1/2 h-fit my-auto  pb-2 foot-font text-center  text-yellow-800'>
                     10
                 </Col>
                 </Row>
