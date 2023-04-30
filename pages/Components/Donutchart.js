1a28dec544d5fa7a7d4d3300b1a0ecc01f0ac2f3
dexb99 <108445718+dexb99@users.noreply.github.com>
2023-04-30 23:27:05 +0700

    db

--- pages/Components/Donutchart.js
+++ pages/Components/Donutchart.js
@@ -1,50 +1,15 @@
 import { useEffect } from "react"
 import { Chart } from "chart.js";
 function Donut() {
-    useEffect(() => {
-        var don = document.getElementById('myChart2').getContext('2d');
-        var myChart2 = new Chart(don, {
-            type: 'doughnut',
-            data: {
-                labels: ["label", "label",],
-                datasets: [{
-                    data: [70, 10,],
-                    borderColor: [
-                        "rgb(75, 192, 192)",
-                        "rgb(255, 205, 86)",
-
-                    ],
-                    backgroundColor: [
-                        "rgb(75, 192, 192, 0.5 )",
-                        "rgb(255, 205, 86, 0.5)",
-
-                    ],
-                    borderWidth: 2,
-                }]
-            },
-            options: {
-                scales: {
-                    xAxes: [{
-                        display: false,
-                    }],
-                    yAxes: [{
-                        display: false,
-                    }],
-                }
-            },
-
-        });
-    }, [])
-
 
     return (
         <>
-            {/* Doughnut chart */}
-            {/* <h1 className="w-[150px] mx-auto mt-10 text-xl font-semibold capitalize ">Doughnut Chart</h1> */}
-
-                <div className='pt-0  w-full h-fit my-auto  pb-2'>
-                    <canvas id='myChart2'></canvas>
+            <div className='pt-0  w-full h-fit my-auto flex pb-2'>
+                <div className="w-1/2">
                 </div>
+                <div className="w-1/2">
+                </div>
+            </div>
 
         </>
     )
